28a344482b3e26677cd280885aef225a5b4b5a28
Tom Xiao <tom.xiao@st.com>
2016-10-31 16:44:26 +0800

    #if 0 #endif Delete Randam Numbers

--- Src/main.c
+++ Src/main.c
@@ -215,9 +215,7 @@ int main(void)
                 STA350BW_Fs_48000,  //uint32_t                samplingFreq,
                 NULL                //void*                   p
                );
-
-  //HAL_Delay(1000);
-
+#if 0
   /* Randam Numbers.
 
   srand(HAL_GetTick());
@@ -227,6 +225,8 @@ int main(void)
     Random_Data[Count] = (int16_t)((rand() % 32768) * sin(HAL_GetTick()));
   }
   */
+#endif
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
